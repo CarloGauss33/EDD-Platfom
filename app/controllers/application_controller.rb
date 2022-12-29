3f51d1f5a6ae05722eb9cbccf64a2dfe6db649a6
Carlos Paredes <cparedesr@uc.cl>
2022-12-28 22:36:30 -0300

    chore(): set pundit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   include PowerTypes::Presentable
+  include Pundit::Authorization
+
+  rescue_from Pundit::NotAuthorizedError, with: :not_authorized
+
+  protect_from_forgery prepend: true, unless: -> { request.format.json? }
+  respond_to :html, :json
+
+  private
+
+  def not_authorized
+    redirect_back(fallback_location: root_path)
+  end
 end
