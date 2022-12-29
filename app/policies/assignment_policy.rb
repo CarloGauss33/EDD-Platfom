c9466d0fb3788b9839becd8f111c82645345f00e
Carlos Paredes <cparedesr@uc.cl>
2022-12-28 22:37:19 -0300

    feat(Assignment): add policy

---
+++ app/policies/assignment_policy.rb
@@ -0,0 +1,15 @@
+class AssignmentPolicy < ApplicationPolicy
+  def show?
+    user.present? && assignment_active?
+  end
+
+  private
+
+  def assignment_on_time?
+    record.start_date < Time.zone.now && record.end_date > Time.zone.now
+  end
+
+  def assignment_active?
+    record.active? && assignment_on_time?
+  end
+end
