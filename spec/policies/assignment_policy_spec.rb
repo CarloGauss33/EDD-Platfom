6131a17c6794e86e395e53c0a7a9ee355558e16b
Carlos Paredes <cparedesr@uc.cl>
2022-12-28 22:37:19 -0300

    feat(Assignment): add policy

---
+++ spec/policies/assignment_policy_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe AssignmentPolicy, type: :policy do
+  subject { described_class }
+
+  let(:user) { User.new }
+
+  permissions :show? do
+    context 'when user and assignment are valid' do
+      let!(:assignment) { create(:assignment, start_date: 1.day.ago, end_date: 1.day.from_now, status: :active) }
+
+      it 'grants access' do
+        expect(described_class).to permit(user, assignment)
+      end
+    end
+
+    context 'when user and assignment are invalid' do
+      let!(:assignment) { create(:assignment, status: :completed) }
+
+      it 'denies access' do
+        expect(described_class).not_to permit(user, assignment)
+      end
+    end
+
+    context 'when user is invalid' do
+      let!(:assignment) { create(:assignment, start_date: 1.day.ago, end_date: 1.day.from_now, status: :active) }
+
+      it 'denies access' do
+        expect(described_class).not_to permit(nil, assignment)
+      end
+    end
+  end
+end
