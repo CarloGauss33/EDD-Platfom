33e0261eb03ea60c9da017129d512a6869f13f21
Carlos Paredes <cparedesr@uc.cl>
2023-02-06 20:53:19 -0300

    feat(OpenAI): add openai client

---
+++ app/clients/openai_client.rb
@@ -0,0 +1,33 @@
+require 'ruby/openai'
+
+class OpenaiClient
+  OPENAI_API_KEY = ENV.fetch('OPENAI_API_KEY', nil)
+
+  def get_completion(prompt, max_tokens: 250, temperature: 0.1, model: 'text-davinci-003')
+    return nil if prompt.blank?
+    return 'OpenAI not enabled' unless check_openai_enabled?
+
+    result = openai_client.completions(
+      parameters: {
+        model: model,
+        prompt: prompt,
+        temperature: temperature,
+        max_tokens: max_tokens
+      }
+    )
+
+    return nil if result['choices'].blank?
+
+    result['choices'][0]['text']
+  end
+
+  private
+
+  def openai_client
+    @openai_client ||= OpenAI::Client.new(access_token: OPENAI_API_KEY)
+  end
+
+  def check_openai_enabled?
+    ConfigVariable.fetch('OPENAI_ENABLED', false) && OPENAI_API_KEY.present?
+  end
+end
