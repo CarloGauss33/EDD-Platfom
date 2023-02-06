bf35d28cfece7bf347bfcb7a97cab6ecbf6bcd3a
Carlos Paredes <cparedesr@uc.cl>
2023-02-06 00:11:55 -0300

    feat(Serializers): add course to assignment

--- app/serializers/api/internal/assignment_serializer.rb
+++ app/serializers/api/internal/assignment_serializer.rb
@@ -1,12 +1,19 @@
 class Api::Internal::AssignmentSerializer < ActiveModel::Serializer
   type :assignment
+  belongs_to :course
 
   attributes(
     :id,
     :name,
     :description,
+    :status,
     :start_date,
     :end_date,
-    :kind
+    :kind,
+    :course
   )
+
+  def course
+    object.course
+  end
 end
