03360d6e3fa1dd5764868584ab21f1aa8767039a
Carlos Paredes <cparedesr@uc.cl>
2023-02-06 00:14:13 -0300

    fix(Assignments): update controller and views

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -2,24 +2,28 @@ class AssignmentsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @assignment = Assignment.last
+    @assignments = current_user.assignments.order(end_date: :desc).includes(:course)
+  end
+
+  def show
+    @assignment = assignment
     @assignment_questions = assignment_questions
     @course = course
     @assignment_question_responses = assignment_question_responses
   end
 
-  def show
-    @assignment = Assignment.find(params[:id])
-  end
-
   private
 
+  def assignment
+    @assignment = current_user.assignments.find(params[:id])
+  end
+
   def assignment_questions
-    @assignment_questions = @assignment.assignment_questions
+    @assignment_questions = assignment.assignment_questions
   end
 
   def course
-    @course = @assignment.course
+    @course = assignment.course
   end
 
   def student
@@ -27,7 +31,7 @@ class AssignmentsController < ApplicationController
   end
 
   def assignment_response
-    @assignment_response = AssignmentResponse.by_assignment_and_student(@assignment.id, student.id)
+    @assignment_response = AssignmentResponse.by_assignment_and_student(assignment.id, student.id)
   end
 
   def assignment_question_responses
