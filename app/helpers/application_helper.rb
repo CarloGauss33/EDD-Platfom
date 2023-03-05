936c7d69eb6d61e88730e2f8d94bb7ab8b0540f2
Carlos Paredes <cparedesr@uc.cl>
2023-03-04 22:28:45 -0300

    feat(NewSession): replace registration link

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def default_course_student_registration_path
+    new_course_student_path(current_course)
+  end
+
+  private
+
+  def current_course
+    @current_course ||= Course.current
+  end
 end
