38ae8669b605dd9ea1552212b1e5c836abd45858
Carlos Paredes <cparedesr@uc.cl>
2023-03-05 23:48:31 -0300

    fix(): fix sender mail

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,6 @@
 class ApplicationMailer < ActionMailer::Base
   helper :mailer
-  default from: ENV.fetch('DEFAULT_EMAIL_FROM', '')
+  default from: ENV.fetch('DEFAULT_EMAIL_ADDRESS', 'iic2133@ing.puc.cl')
   default cco: ENV.fetch('DEFAULT_EMAIL_CC_RECIPIENTS', '').split(',')
   layout 'mailer'
 end
