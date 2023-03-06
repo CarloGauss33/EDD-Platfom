acdaac77674b74214b2b79420f32fde897040e51
Carlos Paredes <cparedesr@uc.cl>
2023-03-05 22:57:12 -0300

    feat(Mailer): add production configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,7 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+  host = ENV["PRODUCTION_HOST"]
   config.force_ssl = true
   config.cache_classes = true
   config.eager_load = true
@@ -13,6 +14,16 @@ Rails.application.configure do
   config.log_tags = [:request_id]
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: host }
+  config.action_mailer.smtp_settings = {
+    address: ENV["SMTP_ADDRESS"],
+    port: ENV["SMTP_PORT"],
+    user_name: ENV["SMTP_USERNAME"],
+    password: ENV["SMTP_PASSWORD"],
+    authentication: "plain",
+    enable_starttls_auto: true
+  }
   config.i18n.fallbacks = true
   config.active_support.deprecation = :notify
   config.active_support.disallowed_deprecation = :log
@@ -26,5 +37,5 @@ Rails.application.configure do
   end
   config.active_record.dump_schema_after_migration = false
 
-  routes.default_url_options = { host: ENV["PRODUCTION_HOST"], protocol: 'https' }
+  routes.default_url_options = { host: host, protocol: 'https' }
 end
