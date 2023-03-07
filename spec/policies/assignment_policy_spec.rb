1470230740f12075f97dc36c8c2cea0818c6eb26
Carlos Paredes <cparedesr@uc.cl>
2023-03-07 01:08:36 -0300

    test(AssignmentPolicy): increase coverage

--- spec/policies/assignment_policy_spec.rb
+++ spec/policies/assignment_policy_spec.rb
@@ -7,7 +7,12 @@ RSpec.describe AssignmentPolicy, type: :policy do
 
   permissions :show? do
     context 'when user and assignment are valid' do
-      let!(:assignment) { create(:assignment, start_date: 1.day.ago, end_date: 1.day.from_now, status: :active) }
+      let!(:assignment) do
+        create(:assignment,
+               start_date: 1.day.ago,
+               end_date: 1.day.from_now,
+               status: :active)
+      end
 
       it 'grants access' do
         expect(described_class).to permit(user, assignment)
@@ -23,7 +28,84 @@ RSpec.describe AssignmentPolicy, type: :policy do
     end
 
     context 'when user is invalid' do
-      let!(:assignment) { create(:assignment, start_date: 1.day.ago, end_date: 1.day.from_now, status: :active) }
+      let!(:assignment) do
+        create(:assignment,
+               start_date: 1.day.ago,
+               end_date: 1.day.from_now,
+               status: :active)
+      end
+
+      it 'denies access' do
+        expect(described_class).not_to permit(nil, assignment)
+      end
+    end
+  end
+
+  permissions :create? do
+    context 'when user and assignment are valid' do
+      let!(:assignment) do
+        create(:assignment,
+               start_date: 1.day.ago,
+               end_date: 1.day.from_now,
+               status: :active)
+      end
+
+      it 'grants access' do
+        expect(described_class).to permit(user, assignment)
+      end
+    end
+
+    context 'when user and assignment are invalid' do
+      let!(:assignment) { create(:assignment, status: :completed) }
+
+      it 'denies access' do
+        expect(described_class).not_to permit(user, assignment)
+      end
+    end
+
+    context 'when user is invalid' do
+      let!(:assignment) do
+        create(:assignment,
+               start_date: 1.day.ago,
+               end_date: 1.day.from_now,
+               status: :active)
+      end
+
+      it 'denies access' do
+        expect(described_class).not_to permit(nil, assignment)
+      end
+    end
+  end
+
+  permissions :update? do
+    context 'when user and assignment are valid' do
+      let!(:assignment) do
+        create(:assignment,
+               start_date: 1.day.ago,
+               end_date: 1.day.from_now,
+               status: :active)
+      end
+
+      it 'grants access' do
+        expect(described_class).to permit(user, assignment)
+      end
+    end
+
+    context 'when user and assignment are invalid' do
+      let!(:assignment) { create(:assignment, status: :completed) }
+
+      it 'denies access' do
+        expect(described_class).not_to permit(user, assignment)
+      end
+    end
+
+    context 'when user is invalid' do
+      let!(:assignment) do
+        create(:assignment,
+               start_date: 1.day.ago,
+               end_date: 1.day.from_now,
+               status: :active)
+      end
 
       it 'denies access' do
         expect(described_class).not_to permit(nil, assignment)
