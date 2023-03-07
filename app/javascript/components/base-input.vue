da878cbf9f95d6635214552756bbca658b7c9ea6
Carlos Paredes <cparedesr@uc.cl>
2023-03-06 23:35:35 -0300

    feat(BaseInput): change style

--- app/javascript/components/base-input.vue
+++ app/javascript/components/base-input.vue
@@ -66,7 +66,7 @@ export default {
         v-if="$slots.prefix"
         class="flex items-center whitespace-nowrap rounded-l py-3 px-2 text-white"
         :class="[
-          props.disabled ? 'bg-slate-500 text-slate-700 opacity-75' : 'bg-edd-blue-500'
+          props.disabled ? 'bg-slate-300 text-slate-800 opacity-75' : 'bg-edd-blue-500'
         ]"
       >
         <slot name="prefix" />
@@ -83,7 +83,7 @@ export default {
         :required="props.required"
         class="w-full rounded-md px-2 py-3 focus:border-edd-blue-200 focus:outline-none focus:ring-2 focus:ring-edd-blue-500 focus:ring-offset-2"
         :class="[
-          props.disabled ? 'bg-slate-300 border-slate-500 text-slate-700 opacity-75' : 'bg-white',
+          props.disabled ? 'bg-slate-300 border-slate-500 text-black opacity-90' : 'bg-white',
           {
             '!text-red-600 !border-red-600': !!errorMessage,
             'rounded-l-none': $slots.prefix,
