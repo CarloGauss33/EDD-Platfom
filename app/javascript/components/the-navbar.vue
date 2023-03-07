12026845e3f5084035718649522d7c721c4a20ab
Carlos Paredes <cparedesr@uc.cl>
2023-03-07 00:50:54 -0300

    feat(TheNavbar): add navbar

---
+++ app/javascript/components/the-navbar.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { defineProps, withDefaults } from 'vue';
+
+interface Props {
+  user?: User;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  user: undefined,
+});
+
+</script>
+<template>
+  <header class="flex items-center justify-between rounded-2xl px-4 pt-3 opacity-80 md:px-14">
+    <a
+      href="/"
+      class="text-xl font-semibold md:text-2xl"
+    >
+      EDD Platform
+    </a>
+
+    <base-button
+      href="/profile"
+      size="sm"
+      variant="secondary"
+      class="mb-1"
+    >
+      {{ user ? user.fullName : 'Iniciar sesión' }}
+    </base-button>
+  </header>
+</template>
