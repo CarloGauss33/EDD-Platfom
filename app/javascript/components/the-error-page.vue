f952c071009f067e5cc585fddde9949ebd9a36b9
Carlos Paredes <cparedesr@uc.cl>
2023-03-10 22:30:49 -0300

    feat(ErrorComponent): add error page component

---
+++ app/javascript/components/the-error-page.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { defineProps, withDefaults } from 'vue';
+
+interface Props {
+  errorCode: '404' | '500';
+}
+withDefaults(defineProps<Props>(), {
+  errorCode: '404',
+});
+
+</script>
+<template>
+  <div class="flex h-full flex-col items-center justify-center">
+    <div class="text-8xl font-bold">
+      {{ errorCode }}
+    </div>
+    <div class="text-3xl">
+      {{ $t(`errors.${errorCode}`) }}
+    </div>
+    <base-button
+      class="mt-4"
+      href="/"
+    >
+      {{ $t('errors.homeLinkText') }}
+    </base-button>
+  </div>
+</template>
