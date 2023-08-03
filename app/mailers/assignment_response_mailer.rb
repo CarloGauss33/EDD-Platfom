3dae198c7f390fdb59cd56a641cf018a24a61c37
Carlos Paredes <cparedesr@uc.cl>
2023-08-03 16:03:53 -0400

    feat(AssignmentResponse): add mailer

---
+++ app/mailers/assignment_response_mailer.rb
@@ -0,0 +1,17 @@
+class AssignmentResponseMailer < ApplicationMailer
+  def notify_user_response(assignment_response)
+    @assignment_response = assignment_response
+    @user = assignment_response.user
+
+    return if assignment_response.blank?
+
+    assignment_response.assignment_question_responses.each do |response|
+      attachments[response.file.original_filename] = response.file.download
+    end
+
+    mail(
+      to: @user.email,
+      subject: "[EDD] #{assignment_response.assignment_name} Entregada!"
+    )
+  end
+end
